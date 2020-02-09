aa3bbe5b61467620e1e2c05fffc5535acfc2abad
Patrick T. McKee <bludevil@vt.edu>
2020-02-08 22:17:55 -0500

    added scan ability to allow for multiple calculations

--- dat.c
+++ dat.c
@@ -7,12 +7,15 @@ int main(){
 	int dat1[7];
 	int dat2[7];
 
-	dat1[0] = 1;
+	printf( "Enter a value for a: ");
+	scanf("%d", &dat1[4]);
+        printf( "Enter a value for b: ");
+	scanf("%d", &dat1[5]);
+
+	dat1[0] = 1;	
 	dat1[1] = 0;
 	dat1[2] = 0;
 	dat1[3] = 1;
-	dat1[4] = 28056;
-	dat1[5] = 3032;
 	dat1[6] = 0;	
 
 	dat2[0] = 0;
@@ -23,12 +26,14 @@ int main(){
         dat2[5] = 0;
         dat2[6] = 0;
 
+                //print first line of the division algorithm table
+                printf("%10d  %10d  %10d  %10d  %10d  %10d  %10d \n", dat1[0], dat1[1], dat1[2], dat1[3], dat1[4], dat1[5], dat1[6]);
+
+
+
 	//while loop to calculate table values for part 1
 	while (dat1[5] != 0){
-		//print contexts of dat1 spaced apart to create the next
-		//line of the division algorithm table
-		printf("%d  %d  %d  %d  %d  %d  %d \n", dat1[0], dat1[1], dat1[2], dat1[3], dat1[4], dat1[5], dat1[6]);
-
+	
 		//calculate dat2, the values for the next line of the table
 		//must calculate the 7th valye because it is vital to
 		//the calculate of the other values in dat2
@@ -43,7 +48,7 @@ int main(){
                 dat2[3] = dat1[2] - ( dat1[3] * dat2[6] );
                 dat2[4] = dat1[5];
                 dat2[5] = dat1[4] - ( dat1[5] * dat2[6] );
-		
+	
 		//roll over values for next iteration of loop
 	        dat1[0] = dat2[0];
         	dat1[1] = dat2[1];
@@ -52,11 +57,13 @@ int main(){
 	        dat1[4] = dat2[4];
         	dat1[5] = dat2[5];
 	        dat1[6] = dat2[6];
+
+                //print contents of dat1 spaced apart to create the next
+                //line of the division algorithm table
+		printf("%10d  %10d  %10d  %10d  %10d  %10d  %10d \n", dat1[0], dat1[1], dat1[2], dat1[3], dat1[4], dat1[5], dat1[6]);
+                                                
 	}
-
-
-
-
+        
 	return 0;
 
 }
